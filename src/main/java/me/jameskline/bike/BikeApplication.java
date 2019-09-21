acff49a7fa6f34aa6c25a282521dd36e09ad0445
jamesk111 <8887227+jamesk111@users.noreply.github.com>
2019-09-21 14:49:58 -0600

    Initial creation / Add bike model and controller

---
+++ src/main/java/me/jameskline/bike/BikeApplication.java
@@ -0,0 +1,13 @@
+package me.jameskline.bike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeApplication.class, args);
+    }
+
+}
