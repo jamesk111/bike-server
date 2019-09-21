179e347a8c41d58f117a87422ae9c44e1e3e0f69
jamesk111 <8887227+jamesk111@users.noreply.github.com>
2019-09-21 15:02:26 -0600

    add dbs properties and convert bike to JPA entity

--- src/main/java/me/jameskline/bike/models/Bike.java
+++ src/main/java/me/jameskline/bike/models/Bike.java
@@ -1,9 +1,19 @@
 package me.jameskline.bike.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.sql.Date;
 
+@Entity
 public class Bike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String name;
     private String email;
     private String phone;
@@ -12,6 +22,14 @@ public class Bike {
     private BigDecimal purchasePrice;
     private Date purchaseDate;
     private boolean contact;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
